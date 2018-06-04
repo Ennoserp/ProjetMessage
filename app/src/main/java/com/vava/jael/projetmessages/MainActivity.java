3e19c66cf8f65c5f5c4e451107d8d2d162c96c1a
jael.vavasseur@gmail.Com <jael.vavasseur@gmail.Com>
2018-06-04 10:59:13 +0200

    code java

--- app/src/main/java/com/vava/jael/projetmessages/MainActivity.java
+++ app/src/main/java/com/vava/jael/projetmessages/MainActivity.java
@@ -1,14 +1,39 @@
 package com.vava.jael.projetmessages;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnEnvoyer=(Button) findViewById(R.id.send_btn);
+        final EditText numero = (EditText) findViewById(R.id.numero_input);
+        final EditText message = (EditText) findViewById(R.id.texte_input);
+        btnEnvoyer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                @SuppressWarnings("deprecation")
+                public void onClick(View v){
+                    String num = numero.getText().toString();
+                    String msg = message.getText().toString();
+                    if (num.length()>=4 && msg.length()>0) {
+                        SmsManager.getDefault().sendTextMessage(num, null, msg, null, null);
+                        numero.setText("");
+                        message.setText("");
+                    }else{
+                        Toast.makeText(MainActivity.this, "BUG", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
 
     }
 }
