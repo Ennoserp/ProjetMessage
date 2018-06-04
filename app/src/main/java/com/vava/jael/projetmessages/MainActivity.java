2922f3bc10acc49c38ed482d77c714c1d61b8094
jael.vavasseur@gmail.Com <jael.vavasseur@gmail.Com>
2018-06-04 11:07:52 +0200

    code java

--- app/src/main/java/com/vava/jael/projetmessages/MainActivity.java
+++ app/src/main/java/com/vava/jael/projetmessages/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
